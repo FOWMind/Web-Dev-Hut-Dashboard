800f98ddeedab1551749a3e8f81b21616ecb581d
ykIsCoding <Gamerlykesdiary@gmail.com>
2023-07-13 14:09:40 +0800

    changed texts

---
+++ src/admin/app.js
@@ -0,0 +1,63 @@
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+     'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+  translations: {
+    'es': {
+      'Auth.form.welcome.title': 'Bienvenido al dashboard de Web Dev Hut, Collaborador!',
+      'Auth.form.welcome.subtitle': 'ensena tus detalles',
+      // Translate a plugin's key/value pair by adding the plugin's name as a prefix
+      // In this case, we translate the "plugin.name" key of plugin "content-type-builder"
+    },
+    'en': {
+      'Auth.form.welcome.title': 'Welcome to Web Dev Hut\'s Dashboard, Collaborator!',
+      'Auth.form.welcome.subtitle': 'Enter your details',
+      // Translate a plugin's key/value pair by adding the plugin's name as a prefix
+      // In this case, we translate the "plugin.name" key of plugin "content-type-builder"
+    },
+  },
+  themes:{
+    colors: {
+      // Only 5 shade for tone
+      // Navy Blue
+      primary100: '#ac73e6',
+      primary200: '#9736e8',
+      primary500: '#ac73e6',
+    }
+  }
+};
+
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
