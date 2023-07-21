348cb73121bce0b5719ccf3d6ac24ba2288f6192
FOWMind <alwl1@hotmail.com>
2023-07-21 07:52:29 -0300

    add lesson by identifier

--- src/api/lesson/controllers/lesson.js
+++ src/api/lesson/controllers/lesson.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 "use strict";
 
 /**
@@ -7,24 +8,48 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::lesson.lesson", ({ strapi }) => ({
-  async getContent(ctx, next) {
+  async findOne(ctx, next) {
     const { id } = ctx.params;
-    const { query } = ctx;
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
 
-    const entity = await strapi
-      .service("api::lesson.lesson")
-      .findOne(id, query);
+    const lesson = await strapi.service("api::lesson.lesson").findOne(id, {
+      populate: {
+        course: true,
+        // course: {
+        //   fields: ["identifier"],
+        // },
+      },
+      ...sanitizedQueryParams,
+    });
 
-    if (entity.course) {
+    if (lesson.course) {
       const lessonContent = await strapi
         .service("api::lesson.lesson")
-        .lessonContent(`${entity.course.identifier}/${entity.identifier}.md`);
-      entity.content = lessonContent;
+        .lessonContent(`${lesson.course.identifier}/${lesson.identifier}.md`);
+      lesson.content = lessonContent;
     }
 
-    // @ts-ignore
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    // @ts-ignore
-    return this.transformResponse(sanitizedEntity);
+    const sanitizedLesson = await this.sanitizeOutput(lesson, ctx);
+    return this.transformResponse(sanitizedLesson);
+  },
+  async findOneByIdentifier(ctx, next) {
+    const { identifier } = ctx.params;
+
+    const lesson = await strapi.db.query("api::lesson.lesson").findOne({
+      where: { identifier },
+      populate: {
+        course: true,
+      },
+    });
+
+    if (lesson.course) {
+      const lessonContent = await strapi
+        .service("api::lesson.lesson")
+        .lessonContent(`${lesson.course.identifier}/${lesson.identifier}.md`);
+      lesson.content = lessonContent;
+    }
+
+    const sanitizedLesson = await this.sanitizeOutput(lesson, ctx);
+    return this.transformResponse(sanitizedLesson);
   },
 }));
